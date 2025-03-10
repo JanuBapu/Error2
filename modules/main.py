82097a670f124377a13d6c64602c05999415d54c
JanuBapu <142379561+JanuBapu@users.noreply.github.com>
2025-03-10 16:07:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -23,9 +23,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import m3u8
 
 bot = Client("bot",
-             bot_token= "7286340326:AAEWGVOlOgFxuklLXFWuDXn48Lt3Yz2dXs4",
-             api_id= 21705536,
-             api_hash= "c5bb241f6e3ecf33fe68a444e288de2d")
+             bot_token= "7429048456:AAEH21RqF9AuwkjFl7FcnpGtJd4B2_uTsHk",
+             api_id= 22594398,
+             api_hash= "3a2408d97d6a222d87766dac2da302df")
 
 if not os.path.exists("downloads"):
     os.makedirs("downloads")
